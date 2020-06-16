560379d7dfad2b3a15e11d4337b57df7f8873a72
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-06-16 02:38:08 +0000

    주석달기

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int is_odd(int num) {
     return 1; //짝수
   }
   // num이 홀수 ?? 짝수??
-  return 0;
+  return 0; //홀수
 }
 
 // 어떤수가 3의 배수인지 아닌지 체크하는 함수
@@ -21,18 +21,18 @@ int is_three_multiple(int num) {
     return 1; //3의 배수
   }
 
-  return 0;
+  return 0; //3의 배수가 아님
 }
 
 // is_odd와 is_three_multiple 이용해서 완성해주세요.
 int get_sum(int n, int m) {
   int sum = 0;
-  for (int i = n; n <= m; n++) {
+  for (int i = n; n <= m; n++) { //첫번쨰 와 두 번째 수 사이에 있는 수들 구하기
 
-      if(is_three_multiple(n) == 1)
+      if(is_three_multiple(n) == 1) //만약에 3의 배수가 맞다면
       {
-        if(is_odd(n) == 0){
-          sum += n;
+        if(is_odd(n) == 0){ //만약에 홀수가 맞다면
+          sum += n; //수들 더하기
         }
       }
 
